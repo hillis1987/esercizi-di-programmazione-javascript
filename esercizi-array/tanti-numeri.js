7c1168d612f9235ed2cc1f0e0de75785dd1ffb6c
hillis1987 <70323341+hillis1987@users.noreply.github.com>
2020-09-02 23:26:35 +0200

    Update tanti-numeri.js

--- esercizi-array/tanti-numeri.js
+++ esercizi-array/tanti-numeri.js
@@ -12,3 +12,18 @@
 
   http://www.imparareaprogrammare.it
 */
+var a =[3, 5, 10, 2, 8];
+var appo = 0;
+
+for (var i = 0; i < a.length; i++) {
+  appo += a[i];
+}
+
+var media = appo/a.length;
+console.log(media);
+var min = [];
+for (var i = 0; i < a.length; i++) {
+  if (a[i]<media)
+      min.push(a[i]);
+  }
+console.log(min);
