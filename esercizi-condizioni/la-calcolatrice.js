c0a08b35fcfd78b574cb0f880dd4b9e3d8147303
hillis1987 <70323341+hillis1987@users.noreply.github.com>
2020-09-04 00:44:48 +0200

    Update la-calcolatrice.js

--- esercizi-condizioni/la-calcolatrice.js
+++ esercizi-condizioni/la-calcolatrice.js
@@ -28,3 +28,48 @@
 
   http://www.imparareaprogrammare.it
 */
+var a = 5, b = 0;
+var operazione = prompt('Scegli quale operazione vuoi effettuare: somma, sottrazione, moltiplicazione, divisione, modulo, potenza, media');
+switch (operazione) {
+  case 'somma':
+    var somma = a+b;
+    console.log(somma);
+    break;
+  case 'sottrazione':
+    var sottrazione = a-b;
+    console.log(sottrazione);
+    break;
+  case 'moltiplicazione':
+    var moltiplicazione = a*b;
+    console.log(moltiplicazione);
+    break;
+  case 'divisione':
+    if (b == 0) {
+      console.log('calcolo impossibile');
+      break;
+    }
+    var divisione = a/b;
+    console.log(divisione);
+    break;
+  case 'modulo':
+    a = parseInt(a);
+    b = parseInt(b);
+    if (b == 0) {
+      console.log('impossibile effettuare l\'operazione richiesta');
+      break;
+    }
+    var modulo = a%b;
+    console.log(modulo);
+    break;
+
+  case 'potenza':
+    var potenza = a**b;
+    console.log(potenza);
+    break;
+  case 'media':
+    var media = (a+b)/2;
+    console.log(media);
+    break;
+  default:
+    console.log('Operazione non valida, riprovare');
+}
